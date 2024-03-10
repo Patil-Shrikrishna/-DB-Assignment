bcd5b335a15ad5901da951748e6e74ecc556ede3
Patil-Shrikrishna <krishnapt1993@gmail.com>
2024-03-10 13:26:51 +0530

    Deleted Schema.js file

--- Schema.js
+++
@@ -1,7 +0,0 @@
-### Read the diagram carefully and answer the below questions.
-
-![ecommerce table](https://github.com/Patil-Shrikrishna/-DB-Assignment/blob/main/product-management-ecommerce-table-.webp)
-
-**Question**
-
-### 3. Create schema in any Database script or any ORM (Object Relational Mapping).
