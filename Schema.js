9c5f408e72fc21146165cb0ebaf16343ca337750
Patil-Shrikrishna <97384724+Patil-Shrikrishna@users.noreply.github.com>
2024-03-09 22:25:01 -0800

    Create Schema.js

---
+++ Schema.js
@@ -0,0 +1,7 @@
+### Read the diagram carefully and answer the below questions.
+
+![ecommerce table](https://github.com/Patil-Shrikrishna/-DB-Assignment/blob/main/product-management-ecommerce-table-.webp)
+
+**Question**
+
+### 3. Create schema in any Database script or any ORM (Object Relational Mapping).
